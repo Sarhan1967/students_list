4ddc3a8cf5320d97d8facef6b3f714e8f8f692f1
Sarhan1967 <sarhan1967@gmail.com>
2021-12-21 10:34:27 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:students_list/screens/students_list.dart';
+import 'dart:async';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+/*getFileContent() async{
+  String fileContent = await downloadFile();
+  print(fileContent);
+}*/
+
+/*getFileContent() {
+  Future<String> fileContent =  downloadFile();
+  fileContent.then((resut){
+    print(resut);
+  });
+}
+
+Future<String> downloadFile(){
+
+  Future<String> content = Future.delayed(Duration(seconds: 6), () {
+    return "Internet File Content";
+  });
+
+  return content;
+}
+*/
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Students List",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.cyan
+      ),
+      home: StudentsList(),
+    );
+  }
+}
